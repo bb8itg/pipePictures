f4d156905192bbb97f92885e9083f24daff644cb
bb8itg <bb8itg@inf.elte.hu>
2022-11-13 11:50:57 +0100

    update

--- generateAnnotationForPipeRaw.py
+++ generateAnnotationForPipeRaw.py
@@ -5,17 +5,24 @@ f = open('pipe_json.json')
   
 # returns JSON object as 
 # a dictionary
-a = '{"filename":}'
 data = json.load(f)
-  
+normalize = json.load(open('pipe_coco.json'))
 # Iterating through the json
 # list
-classNumber = 1
+widthHeightList = []
+for i in normalize['images']:
+    widthHeightList.append((i['width'],i['height'],i['file_name']))
+
+
+
+classNumber = 81
 for i in data:
-    print(data[i]['filename'].split('.')[0])
-    print(classNumber, data[i]['regions'][0]['shape_attributes']['x'], data[i]['regions'][0]['shape_attributes']['y'], data[i]['regions'][0]['shape_attributes']['width'], data[i]['regions'][0]['shape_attributes']['height'])
+    w = h = 0.0
+    for j in widthHeightList:
+        if j[2].split('.')[0] == data[i]['filename'].split('.')[0]:
+            w = float(j[0])
+            h = float(j[1])
     f = open(("generatedAnnotation/"+data[i]['filename'].split('.')[0]+".txt"), "w")
-    f.write(str(classNumber) + " " + str(data[i]['regions'][0]['shape_attributes']['x']) + " " + str(data[i]['regions'][0]['shape_attributes']['y']) + " " + str(data[i]['regions'][0]['shape_attributes']['width']) + " " + str(data[i]['regions'][0]['shape_attributes']['height']))
-    f.close()
+    f.write(str(classNumber) + " " + format(float(data[i]['regions'][0]['shape_attributes']['x'])/w, '.6f') + " " + format(float(data[i]['regions'][0]['shape_attributes']['y'])/h, '.6f') + " " + format(float(data[i]['regions'][0]['shape_attributes']['width'])/w, '.6f') + " " + format(float(data[i]['regions'][0]['shape_attributes']['height'])/h, '.6f'))
 # Closing file
 f.close()
